f2eb0db5fdf166e70031fd1fc14d2b2e912930bc
Ritika Mukherjee <101868439+RitikaMu@users.noreply.github.com>
2023-03-02 19:50:47 +0530

    Update Pad.js

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {useRef} from "react";
 import {bank1} from "./App"
 
 function Pad({handleClick, power, backgroundStyle, element, id }) {
+    const audio = new audio(bank1[element].source);
+    
+    window.addeventListener("keydown", (event) => {
+        if(event.key === element || event.key === element.toLowerCase()) {
+            handleClick(bank1[element]);
+            audio.play();
+        }
+    });
     return (
       <button 
         data-tag={id}
         type='button'
         className='drum-pad' 
-        onClick={handleClick} 
+        onClick={() => {
+            audio.play();
+            handleClick(bank1[elemenr]);
+        }} 
         id={bank1[element]} 
         disabled={!power} 
-        style={{background: `${backgroundStyle}`}}>
+        style={{background: `${backgroundStyle}`}}
+        >
         {element}
         <audio id={element} src={bank1[element].source} className='clip'></audio>
       </button>
